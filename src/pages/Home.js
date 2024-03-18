d7795f6153913f3e283b715a8efc23a917199c8d
tammytran96 <tammytran96@gmail.com>
2024-03-18 15:28:46 -0700

    style landing/home section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import '../styles.css';
 
 const Home = () => {
   return (
     <>
-      <h2>tammyTran = ['developer', 'designer'],</h2>
-      <h3>// full-stack developer who loves to learn and make cool things! Add more later :)</h3>
+      <div className="home-container">
+        <main className="home-main">
+          <div className="home-content-wrapper">
+            <h1 className="home-text-title">
+              tammyTran = ['developer', 'designer'];
+            </h1>
+            <h3 className="home-text-description">
+              // full-stack developer who loves to learn and make cool things!
+              Add more later
+            </h3>
+          </div>
+        </main>
+      </div>
     </>
   );
 };
